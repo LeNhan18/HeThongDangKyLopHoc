ba24399a424b0393dd2e935101b2707e6856775d
LeThanhNhan <lnhan8132@gmail.com>
2025-07-14 19:27:16 +0700

    tạo model course

---
+++ app/models/course.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from app.database import Base
+
+class Course(Base):
+    __tablename__ = "courses"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), index=True, nullable=False)
+    description = Column(String(1000))
+    classes = relationship("Class", back_populates="course") 
