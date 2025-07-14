fab5d562309feeface03a4fd32abda1658b930dc
LeThanhNhan <lnhan8132@gmail.com>
2025-07-14 19:27:42 +0700

    tạo model history

---
+++ app/models/history.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from app.database import Base
+
+class ClassHistory(Base):
+    __tablename__ = "class_histories"
+    id = Column(Integer, primary_key=True, index=True)
+    class_id = Column(Integer, ForeignKey("classes.id"))
+    changed_by = Column(Integer, ForeignKey("users.id"))
+    change_type = Column(String(255))
+    change_time = Column(DateTime, default=datetime.utcnow)
+    note = Column(String(1000), nullable=True)
+    class_ = relationship("Class")
+    user = relationship("User") 
