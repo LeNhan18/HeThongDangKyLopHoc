dfdeae822d4230112bfbb465868d7c793ea1789a
LeThanhNhan <lnhan8132@gmail.com>
2025-07-18 23:29:17 +0700

    Tách để service xử lý nghiệp vụ course

--- app/api/endpoints_course.py
+++ app/api/endpoints_course.py
@@ -2,22 +2,22 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.database import get_db
 from app.schemas.course import Course, CourseCreate
-from app.CRUD import create_course, get_courses, get_course
+from app.services import course_service
 from app.models.course import Course as CourseModel
 
 router = APIRouter()
 
 @router.post("/courses/", response_model=Course)
 def create_new_course(course: CourseCreate, db: Session = Depends(get_db)):
-    return create_course(db, course)
+    return course_service.create_course(db, course)
 
 @router.get("/courses/", response_model=list[Course])
 def list_courses(db: Session = Depends(get_db)):
-    return get_courses(db)
+    return course_service.get_courses(db)
 
 @router.get("/courses/{course_id}", response_model=Course)
 def get_course_detail(course_id: int, db: Session = Depends(get_db)):
-    course = get_course(db, course_id)
+    course = course_service.get_course(db, course_id)
     if not course:
         raise HTTPException(status_code=404, detail="Không tìm thấy khóa học")
     return course
