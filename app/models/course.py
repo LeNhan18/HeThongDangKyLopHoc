c5481fc7f7a942e45ea4e49c2ad38a0f3f73c10e
LeThanhNhan <lnhan8132@gmail.com>
2025-07-26 21:26:34 +0700

    Edit khóa học

--- app/models/course.py
+++ app/models/course.py
@@ -7,5 +7,5 @@ class Course(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(255), index=True, nullable=False)
     description = Column(String(1000))
-    classes = relationship("Class", back_populates="course")
     image = Column(String(1000),nullable=True)
+    classes = relationship("Class", back_populates="course")
